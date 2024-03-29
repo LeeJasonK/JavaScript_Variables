bc6404868435267a112a8469d5b3034b7151f3f2
Jason Lee <nblueyi55@gmail.com>
2024-03-28 21:37:07 -0500

    updated

--- app.js
+++ app.js
@@ -4,7 +4,36 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = "Bruce";
+lastName = "Lee";
+age = 40;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+// OR console.log(firstName, lastName, age);
 
 // Exercise 3
+let language = "javaScript";  //string
+let createdYear = 1995;       //number
+let isCaseSensitive = true;   //Boolean
+
+// Exercise 4
+let price = 1999;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let instock = false;
+let selectedSize = "M";
+console.log(price, isOnSale, selectedSize);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(title, bookmark, hasRead);
